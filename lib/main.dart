4c357303b11b66a4fb3158c4268c0e5b341ee5e9
Anurag Bishnoi <bishnoi_anurag@hotmail.com>
2024-01-19 17:40:13 +0530

    added logic2

--- lib/main.dart
+++ lib/main.dart
@@ -5,44 +5,24 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          primarySwatch: Colors.cyan,
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.transparent,
-          ),
-          textButtonTheme: TextButtonThemeData(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      Color.fromARGB(169, 8, 207, 101))))),
-      home: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          title: Center(
-            child: Container(
-              child: Row(children: [
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.settings_backup_restore)),
-              ]),
-            ),
-          ),
-        ),
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/wood board.jpg"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Game(key: key),
+        primarySwatch: Colors.cyan,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
         ),
       ),
+      home: const Game(),
     );
   }
 }
--- lib/program_logic.dart
+++ lib/program_logic.dart
@@ -1,51 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe/logic2.dart';
 
 class Game extends StatefulWidget {
   const Game({super.key});
 
   @override
-  State<Game> createState() => _GameState();
+  State<Game> createState() => GameState();
 }
 
-int turn = 0;
-List<int> num = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-Image ximage = const Image(image: AssetImage("assets/x.png"));
-Image oimage = const Image(image: AssetImage("assets/o.png"));
-Image? visibleImage;
+class GameState extends State<Game> {
+  int turn = 0;
+  int totalcount = 0;
+  Image ximage = const Image(image: AssetImage("assets/x.png"));
+  Image oimage = const Image(image: AssetImage("assets/o.png"));
 
-class _GameState extends State<Game> {
+  static List<Image?> visibleImage = List<Image?>.filled(9, null);
+  static List<Image?> replacementImage = List<Image?>.filled(9, null);
+
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-      itemBuilder: (context, index) {
-        return GestureDetector(
-            child: Container(
-              decoration: BoxDecoration(border: Border.all()),
-              child: visibleImage,
-            ),
-            onTap: () {
-              ontapped(index);
-            });
-      },
-      itemCount: 9,
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/wood board.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+          itemBuilder: (context, index) {
+            return GestureDetector(
+                child: Container(
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: visibleImage[index],
+                ),
+                onTap: () {
+                  ontapped(index);
+                });
+          },
+          itemCount: 9,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            visibleImage.setAll(0, replacementImage);
+          });
+        },
+        backgroundColor: Colors.orange[900],
+        child: const Icon(Icons.refresh_outlined),
+      ),
     );
   }
 
   void ontapped(int index) {
-    if (turn == 0 && num[index] == 0) {
+    if (totalcount >= 5) {
+      logic();
+    }
+    if (turn == 0 && visibleImage[index] == null) {
       setState(() {
-        num[index] = 1;
-        visibleImage = ximage;
+        visibleImage[index] = ximage;
         turn = 1;
+        totalcount++;
       });
     }
-    if (turn == 1 && num[index] == 0) {
+    if (turn == 1 && visibleImage[index] == null) {
       setState(() {
-        num[index] = 2;
-        visibleImage = oimage;
+        visibleImage[index] = oimage;
         turn = 0;
+        totalcount++;
       });
     }
   }
